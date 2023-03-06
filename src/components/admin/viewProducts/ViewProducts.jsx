805ba28d3a993dc79a9b43713fd069344d72dd50
Osman Abdullayev <os.abdullayev@gmail.com>
2023-03-06 21:29:44 +0400

    starbucks adjustments

--- src/components/admin/viewProducts/ViewProducts.jsx
+++ src/components/admin/viewProducts/ViewProducts.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import { db } from "../../../firebase/config";
 
 const ViewProducts = () => {
-  return (
-    <div>ViewProducts</div>
-  )
-}
+	const [products, setProducts] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-export default ViewProducts
+	useEffect(() => {
+		getProducts();
+	}, []);
+
+	const getProducts = () => {
+		setIsLoading(true);
+
+		try {
+			const productsRef = collection(db, "products");
+			const q = query(productsRef, orderBy("createdAt"));
+
+			onSnapshot(q, (snapshot) => {
+				console.log(snapshot);
+			});
+		} catch (error) {
+			setIsLoading(false);
+			toast.error(error.mesage);
+		}
+	};
+
+	return <div>ViewProducts</div>;
+};
+
+export default ViewProducts;
